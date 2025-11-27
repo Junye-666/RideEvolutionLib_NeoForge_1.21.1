a7656086884bbc1b85d70edaee817206412bc436
Junye <d_d5du@163.com>
2025-11-27 15:02:44 +0100

    Initial commit

---
+++ src/main/java/com/jpigeon/rideevolutionlib/RideEvolutionLibClient.java
@@ -0,0 +1,22 @@
+package com.jpigeon.rideevolutionlib;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+@Mod(value = RideEvolutionLib.MODID, dist = Dist.CLIENT)
+@EventBusSubscriber(modid = RideEvolutionLib.MODID, value = Dist.CLIENT)
+public class RideEvolutionLibClient {
+    public RideEvolutionLibClient(ModContainer container) {
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+    }
+}
